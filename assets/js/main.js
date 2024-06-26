e459ba746535e3b60b90bc28a34e62e01cbc9e3e
Minsung Kim <tkfpsk@gmail.com>
2024-06-26 09:46:10 +0100

    contact

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,7 +30,7 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 /*=============== SHADOW HEADER ===============*/
 const shadowHeader = () =>{
     const header = document.getElementById('header')
-    this.scrollY >= 50 ? header.classList.add('shawdow-header')
+    this.scrollY >= 50 ? header.classList.add('shadow-header')
                        : header.classList.remove('shadow-header')
 }
 window.addEventListener('scroll', shadowHeader)
